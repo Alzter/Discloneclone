008585a0e1eae579907a4b819b5ea924a5f402ee
Alzter <alexsmall713@gmail.com>
2025-05-25 11:58:55 +1000

    implement conv -> dataset method

--- src/agent.py
+++ src/agent.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from typing import Literal
-import sys
 import parser
 from utils.utils import LocalPLM, LocalModelArguments
 from dataclasses import dataclass, field
+from tqdm import tqdm
 
 @dataclass
 class ChatUnderstanding():
@@ -54,7 +54,7 @@ class Agent:
 
         return prompt_template
 
-    def understand_conversation(self, messages : pd.DataFrame, target_user : str, context : str | None = None, context_role : Literal["system", "user"] = "system", tokens:int=128) -> dict:
+    def understand_conversation(self, messages : pd.DataFrame, target_user : str, context : str | None = None, context_role : Literal["system", "user"] = "system", tokens:int=128) -> ChatUnderstanding:
         """
         Use NLP to understand the meaning of a Discord conversation history from a third-person perspective.
 
@@ -97,7 +97,7 @@ class Agent:
 
         return {"interest":interest,"relationship":relationship,"topic":topic}
 
-    def understand_conversation_pov(self, messages : pd.DataFrame, target_user : str, context : str | None = None, context_role : Literal["system", "user"] = "system", tokens:int=128) -> dict:
+    def understand_conversation_pov(self, messages : pd.DataFrame, target_user : str, context : str | None = None, context_role : Literal["system", "user"] = "system", tokens:int=128) -> ChatUnderstanding:
         """
         Use NLP to understand the meaning of a Discord conversation history from the perspective of a given user in first-person.
 
@@ -141,6 +141,120 @@ class Agent:
         #return f"Conversation topic:\n{topic}\n\nMy relationship with {other_users}:\n{relationship}\n\nMy interest in the conversation:\n{interest}"
         # return {"interest":interest,"relationship":relationship,"topic":topic}
         return ChatUnderstanding(interest=interest,relationship=relationship,topic=topic)
+
+    def predict_thought(self, conversation : pd.DataFrame, message_id : int, context="context", tokens : int = 128):
+        conversation = conversation.reset_index(drop=True)[:message_id + 1] 
+        target_message = conversation.iloc[message_id]
+        target_text = target_message.Content
+        target_user = target_message.Author
+        other_users = " and ".join([i for i in conversation.Author.unique() if not i == target_user])
+        
+        context = self.understand_conversation_pov(conversation, target_user)
+        
+        thought_prompt = f"""
+Your name is {target_user}. You are in a text conversation with {other_users}.
+Read the conversation, then tell me what you are thinking as you say:
+'{target_text}'. Answer in first-person tense. Be succinct.""".strip()
+        
+        thought_prompt = self.gen_prompt(conversation, thought_prompt, context=context)
+        
+        predicted_thought = self.model.generate(thought_prompt, temperature=1, max_new_tokens=tokens).text
+        
+        return predicted_thought
+
+    def conversation_to_dataset(self, conversation : pd.DataFrame, target_user : str, batch_size : int = 10, use_context : bool = True, thinking_tokens : int = 0) -> pd.DataFrame:
+        """
+        Convert a Discord conversation into a supervised chat dataset from the perspective of a given user.
+        This can be used to predict messages from a given user (i.e., training a model to impersonate you).
+        
+        Conversations are split up into smaller batches to reduce the size of each input text.
+        
+        Optionally, you can allow an LLM to guess what the target user was thinking for each message.
+        This feature is aimed to improve LLM response precision by getting in the head of the target user.
+        
+        Args:
+            conversation (DataFrame): The conversation to convert.
+            target_user (str): Which user we're trying to predict the messages of.
+            batch_size (int, optional): Maximum number of new input messages per sample. Defaults to 10.
+            use_context (bool, optional): If enabled, at the start of each new batch, a summarisation of the previous batch's conversation is given as context.
+                                          This helps eliminate loss of semantic meaning when slicing conversations into chunks of arbitrary size.
+            thinking_tokens (int, optional): If > 0, predicts the thoughts of the target user for each message using a given number of tokens. Defaults to 0.
+        """
+        data = {"content" : [], "label" : []}
+        
+        # Get the index of each message
+        indices = list(conversation.index)
+        
+        # Slice indices into batches / chunks
+        chunks = [indices[i:i + batch_size] for i in range(0, len(indices), batch_size)]
+        
+        # If we want to predict the target user's thoughts
+        # for each message, we should first gauge what the
+        # relationship between the users is like for all
+        # messages in the conversation. We can then use
+        # this relationship info as context for the
+        # thought prediction prompt to improve its accuracy.
+        full_context = "" 
+        if thinking_tokens > 0:
+            full_context = self.understand_conversation_pov( conversation, target_user=target_user).relationship 
+        # Create an empty context for now
+        context_str = None
+        
+        # For each batch
+        for i, indices in enumerate(tqdm(chunks, "Parsing conversation batches", position=0)):
+            start_index = indices[0]
+            
+            # Get the indices of the target user's messages
+            user_indices = conversation.iloc[indices]
+            user_indices = user_indices[user_indices.Author == target_user].index
+            user_indices = list(user_indices)
+            
+            # For each user message
+            for index in tqdm(user_indices, "Parsing messages", position=1):
+                # Get all messages which preceded it in the batch as a string
+                s = start_index
+                if start_index == index: s -= 1
+                if s < 0: continue
+                inputs = conversation.iloc[s:index]
+                inputs = parser.to_string(inputs, header=True, context=context_str, target_user=target_user)
+                 
+                # Get the user message itself as a string
+                output = conversation.iloc[index:index+1]
+                output = parser.to_string(output, header=False, target_user=target_user)
+                
+                # Get the user's thought for the given message
+                if thinking_tokens > 0:
+                    
+                    # We have to get the index of the user's message relative to
+                    # the start of the batch for .iloc[] to work inside the batch
+                    local_index = index - start_index
+                    
+                    # Create a context for the user's thought for the message
+                    # using their relationship with the other users + conversation history
+                    thinking_context = full_context + "\n" + context_str if context_str else full_context
+                    
+                    # Predict the user's thought for the message
+                    thought = self.predict_thought( conversation.iloc[indices], local_index, thinking_context, tokens=thinking_tokens)
+                    
+                    # Enclose the thought in <thinking> tags
+                    output = f"<thinking>{thought}</thinking>\n\n" + output
+                
+                data['content'].append(inputs)
+                data['label'].append(output)
+                
+                # print("\n----\nIN:")
+                # print(inputs)
+                # print("\n----\nOUT:")
+                # print(output)
+                # print("----")
+            
+            # At the end of each batch, summarise what was discussed
+            # to use as the context string for the next batch.
+            # (Only do this if there are more chunks remaining)
+            if i < len(chunks) - 1 and use_context:
+                context_str = self.understand_conversation_pov( conversation.iloc[indices], context=context_str, target_user=target_user ).topic
+            
+        return pd.DataFrame(data)
 
     def reply(self, messages : pd.DataFrame, context : str | None = None) -> str:
         """
